d75efa6e263e36643020c4ef32bdc0466bcbcb01
Suvink <hello@suvink.me>
2020-04-15 01:58:07 +0530

    Minor Changes

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,7 +2,7 @@
   <div class="root">
     <div class="container has-text-centered">
         <img src="https://media2.giphy.com/media/UuJvftgDkcgSI/source.gif">
-        <h1 class="title is-1">Welcome to Volunteer<Strong class="is-primary brand">ME</Strong></h1>
+        <h1 class="title is-1 mb-0">Welcome to Volunteer<Strong class="is-primary brand">ME</Strong></h1>
     </div>
   </div>
 
